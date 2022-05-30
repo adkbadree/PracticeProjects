cf1bd10d1266663581260496c5513bc6e45db20c
Badri Adhikari <adkbadree@gmail.com>
2022-05-30 07:06:57 -0400

    For loop added

--- src/ForLoop/ForLoopExercise.java
+++ src/ForLoop/ForLoopExercise.java
@@ -1,2 +1,19 @@
-package ForLoop;public class ForLoopExercise {
+package ForLoop;
+
+public class ForLoopExercise {
+
+    public static void main(String[] args) {
+
+        // for loop code for (statement1; statement2, statement3){ CODE TO BE EXECUTED}
+        for(int i = 0; i<=10; i++){
+            System.out.println("I am a programmer");
+        }
+
+        int [] grades = {100, 98, 97, 90, 96, 98}; // index for array datatype [] name = {PARAMATERS}
+        for (int i = 0; i<grades.length;i++ ){
+         System.out.println(grades[i]);
+
+        }
+
+    }
 }
