9d6726aea86b5045260204f357d0cdabfabdaeea
Badri Adhikari <adkbadree@gmail.com>
2022-05-31 18:38:55 -0400

    Updated with Methods

---
+++ src/Methods/Main.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Methods.welcome();
+        Methods.multiply(5, 5);
+        Methods.multiply(3, 2);
+        Methods.multiply(5, 8);
+        Methods.bye();
+
+    }
+}
---
+++ src/Methods/Methods.java
@@ -0,0 +1,24 @@
+package Methods;
+
+public class Methods {
+
+    int a;
+    int b;
+
+    public static void multiply(int a, int b){
+
+        System.out.println(a*b);
+
+
+    }
+    public static void welcome (){
+        System.out.println("Welcome to my Calculator");
+
+    }
+    public static void bye(){
+
+        System.out.println("See you next time");
+    }
+
+
+}
